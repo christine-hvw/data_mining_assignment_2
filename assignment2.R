dca54ac0010a6b3356c066534fcdc3b030adbb27
christine-hvw <chvwesternhagen@outlook.de>
2022-10-31 18:04:19 +0100

    partitioning; feature importance; random forest
    change partitioning back to original folds; add feature importance functions (mnb, lasso); add separate utils script; grid search for random forests; output tuning param. values; RESULTS NOT UPDATED YET

--- assignment2.R
+++ assignment2.R
@@ -16,25 +16,7 @@ library(rpart)         # class. trees
 library(doParallel)    # parallel processing for RFs
 library(randomForest)  # random forests
 
-# function to perform lemmatization with tm
-get_lemmas <- function(x) {
-  lemmas <- udpipe(words(x), "english")[["lemma"]]
-  paste(lemmas, collapse = " ")
-}
-
-# function to return unigrams and bigrams of a document
-get_unibigrams <- function(x) {
-  ngrams <- ngrams(words(x), 1:2)
-  return(unlist(lapply(ngrams, paste, collapse = " "), 
-                use.names = FALSE))
-}
-
-# function to return all pre-processed VCorpus content as words vector
-get_allwords <- function(x) {
-  content_list <- lapply(x, `[[`, 1)
-  content_all <- paste(unlist(content_list), collapse = " ")
-  content_words <- words(content_all)
-}
+source("utils.R")
 
 
 # Data pre-processing -----------------------------------------------------
@@ -60,9 +42,8 @@ revs_all <- tm_map(revs_all, stemDocument)
 # Create label vector (0=deceptive, 1=truthful)
 labels <- c(rep(0, 400), rep(1, 400))
 
-# Data partitioning
-set.seed(123)
-train_partition <- createDataPartition(labels, p = 0.8, list = FALSE)
+# Data partitioning (folds 1-4 out of 5 for both classes)
+train_partition <- c(1:320, 401:720)
 
 # Create document term matrix (dtm)
 dtm_train <- DocumentTermMatrix(revs_all[train_partition])
@@ -70,7 +51,7 @@ dtm_train # -> 4.9k features, 99% sparsity
 
 # Remove sparse terms
 dtm_train <- removeSparseTerms(dtm_train, .99)
-dtm_train # -> 1042 features, 95% sparsity
+dtm_train # -> 1078 features, 95% sparsity
 
 # Create dtm for test set 
 dtm_test <- DocumentTermMatrix(revs_all[-train_partition],
@@ -85,7 +66,7 @@ dtm2_train # -> 44k features, 100% sparsity
 
 # try to set to .99 percent -> make extra training set
 dtm2_train <- removeSparseTerms(dtm2_train, .99)
-dtm2_train # -> 1530 features, 96% sparsity
+dtm2_train # -> 1568 features, 96% sparsity
 
 dtm2_test <- DocumentTermMatrix(revs_all[-train_partition],
                                 list(dictionary = dimnames(dtm2_train)[[2]]))
@@ -95,36 +76,24 @@ dtm2_test <- DocumentTermMatrix(revs_all[-train_partition],
 # compute mutual information of features and class labels and generate ranking
 # (maybe reduce feature set to k most important for modeling)
 
-# dtm_train_decep <- as.matrix(dtm_train)[dimnames(dtm_train)[[1]] %in% 1:400,]
-# dtm_train_true <- as.matrix(dtm_train)[dimnames(dtm_train)[[1]] %in% 401:800,]
-
 # convert document term matrix to binary
-# dtm_decep_bin <- as.matrix(dtm_train_decep) > 0
-# dtm_true_bin <- as.matrix(dtm_train_true) > 0
 dtm_train_bin <- as.matrix(dtm_train) > 0
 
 # compute mutual information of each term with class label
 mi_train <- apply(as.matrix(dtm_train_bin), 2,
-                        function(x,y) {
-                          mi.plugin(table(x, y)/length(y), unit = "log2")
-                          },
-                        labels[train_partition])
+                  function(x,y) {
+                    mi.plugin(table(x, y)/length(y), unit = "log2")
+                    },
+                  labels[train_partition])
 
 # sort the indices from high to low mutual information
-#mi_train_ord <- order(mi_train, decreasing = TRUE)
+mi_train_ord <- order(mi_train, decreasing = TRUE)
 
 # get top five features
-#mi_train[mi_train_ord[1:5]]
+cat("Top 5 features (mutual information)",
+    capture.output(mi_train[mi_train_ord[1:5]]),
+    file = "results/results.txt", sep = "\n", append = TRUE)
 
-# get top five features for deceptive and truthful reviews
-words_decep <- get_allwords(revs_all[1:400])
-words_true <- get_allwords(revs_all[401:800])
-
-mi_decep <- mi_train[names(mi_train) %in% words_decep]
-mi_true <- mi_train[names(mi_train) %in% words_true]
-
-mi_decep[order(mi_decep, decreasing = TRUE)[1:50]]
-mi_true[order(mi_true, decreasing = TRUE)[1:50]]
 
 # Modeling ----------------------------------------------------------------
 # Try 4 different approaches below, each with and without bigram features
@@ -137,51 +106,9 @@ mi_true[order(mi_true, decreasing = TRUE)[1:50]]
 
 ### Hyperparameter tuning -------------------------------------------------
 
-set.seed(123)
-cv_folds <- crossv_kfold(data.frame(as.matrix(dtm_train)), k = 10)
+best_laplace <- tune_mnb(dtm_train)
 
-laplace_vals <- seq(0.1, 1, by = 0.1)
-
-# Training
-models <- list()
-
-models <- lapply(cv_folds$train, function(fold) {
-  lapply(laplace_vals, function(l) {
-    multinomial_naive_bayes(x = as.matrix(fold$data[fold$idx,]), 
-                            y = as.factor(labels[fold$idx]),
-                            laplace = l)
-  })
-})
-
-# Testing
-predictions <- list()
-
-predictions <- lapply(models, function(m) {
-  lapply(1:length(laplace_vals), function(l) {
-    predict(m[[l]], as.matrix(as.data.frame(cv_folds$test[[l]])))
-  })
-})
-
-accuracies <- list()
-
-accuracies <- lapply(1:length(predictions), function(fold) {
-  lapply(1:length(laplace_vals), function(l) {
-    mean(predictions[[fold]][[l]] == labels[cv_folds$test[[fold]][["idx"]]])
-  })
-})
-
-# Average accuracy per Laplace value over folds 
-ave_accuracies <- rowMeans(
-  # bind sub-lists into one data frame
-  do.call("rbind", 
-          # make sub-lists into data frame columns
-          lapply(accuracies, function(x) do.call("cbind", x))
-  )
-)
-
-# identify best value for Laplace smoothing parameter 
-best_laplace <- laplace_vals[ave_accuracies == max(ave_accuracies)]
-
+best_laplace2 <- tune_mnb(dtm2_train)
 
 ### Model training and validation -----------------------------------------
 
@@ -191,32 +118,38 @@ mnb_train <- multinomial_naive_bayes(x = as.matrix(dtm_train),
                                      y = as.factor(labels[train_partition]),
                                      laplace = best_laplace)
 
-mnb_predict <- predict(mnb_train, as.matrix(dtm_test))
+mnb_predict <- predict(mnb_train, as.matrix(dtm_test), type = "prob")
 
+mnb_5features <- get_features_mnb(mnb_train)
+  
 cat("MNB Unigrams",
+    paste("Laplace smoothing param. = ", best_laplace),
     capture.output(
       confusionMatrix(as.factor(mnb_predict), as.factor(labels[-train_partition]),
                       mode = "everything")),
+    paste("Deceptive features:", mnb_5features[1]),
+    paste("Truthful features:", mnb_5features[2]),
     file = "results/results.txt", sep = "\n", append = TRUE)
 
-
-# get top five features (based on Mutual Information, entropy..., slide 42 ff)
 
 #### With bigrams
 
 mnb2_train <- multinomial_naive_bayes(x = as.matrix(dtm2_train), 
                                       y = as.factor(labels[train_partition]),
-                                      laplace = best_laplace)
+                                      laplace = best_laplace2)
 
 mnb2_predict <- predict(mnb2_train, as.matrix(dtm2_test))
 
+mnb2_5features <- get_features_mnb(mnb2_train)
+
 cat("MNB Bigrams",
+    paste("Laplace smoothing param. = ", best_laplace2),
     capture.output(
       confusionMatrix(as.factor(mnb2_predict), as.factor(labels[-train_partition]),
                       mode = "everything")),
+    paste("Deceptive features:", mnb2_5features[1]),
+    paste("Truthful features:", mnb2_5features[2]),
     file = "results/results.txt", sep = "\n", append = TRUE)
-
-# get top five features (based on Mutual Information, entropy..., slide 42 ff)
 
 
 ## LASSO logistic regression (discriminative linear classifier)------------
@@ -233,10 +166,15 @@ lasso_train <- cv.glmnet(x = as.matrix(dtm_train),
 lasso_predict <- predict(lasso_train, newx = as.matrix(dtm_test), 
                          s = "lambda.1se", type = "class")
 
+lasso_5features <- get_features_lasso(lasso_train, dtm_test)
+
 cat("LASSO Unigrams",
+    paste("lambda (1 SE) =", lasso_train[["lambda.1se"]]),
     capture.output(
       confusionMatrix(as.factor(lasso_predict), as.factor(labels[-train_partition]),
                       mode = "everything")),
+    paste("Deceptive features:", lasso_5features[1]),
+    paste("Truthful features:", lasso_5features[2]),
     file = "results/results.txt", sep = "\n", append = TRUE)
 
 ### Bigrams
@@ -248,10 +186,15 @@ lasso2_train <- cv.glmnet(x = as.matrix(dtm2_train),
 lasso2_predict <- predict(lasso2_train, newx = as.matrix(dtm2_test), 
                           s = "lambda.1se", type = "class")
 
+lasso2_5features <- get_features_lasso(lasso2_train, dtm2_test)
+
 cat("LASSO Bigrams",
+    paste("lambda (1 SE) =", lasso2_train[["lambda.1se"]]),
     capture.output(
       confusionMatrix(as.factor(lasso2_predict), as.factor(labels[-train_partition]),
                       mode = "everything")),
+    paste("Deceptive features:", lasso2_5features[1]),
+    paste("Truthful features:", lasso2_5features[2]),
     file = "results/results.txt", sep = "\n", append = TRUE)
 
 
@@ -271,6 +214,7 @@ tree_train <- train(x = as.matrix(dtm_train),
 tree_predict <- predict(tree_train, as.matrix(dtm_test))
 
 cat("Tree Unigrams",
+    paste("cp =", tree_train[["bestTune"]][["cp"]]),
     capture.output(
       confusionMatrix(as.factor(tree_predict), as.factor(labels[-train_partition]),
                       mode = "everything")),
@@ -288,6 +232,7 @@ tree2_train <- train(x = as.matrix(dtm2_train),
 tree2_predict <- predict(tree2_train, as.matrix(dtm2_test))
 
 cat("Tree Bigrams",
+    paste("cp =", tree2_train[["bestTune"]][["cp"]]),
     capture.output(
       confusionMatrix(as.factor(tree2_predict), as.factor(labels[-train_partition]),
                       mode = "everything")),
@@ -297,92 +242,101 @@ cat("Tree Bigrams",
 ## Random forest (ensemble of non-linear classifiers)----------------------
 # hyperparameters: - numbers of parameters to sample from (mtry), 
 # - number of trees (ntree) (not in caret, need to loop over ntree)
-# 
+
 ### Unigrams
 
-# try adaptive resampling for ntree
-start.time <- proc.time()
-cl <- makePSOCKcluster(6)
-registerDoParallel(cl)
+# mtry grid centered around sqrt(nfeat) ~ 32
+mtry_grid <- data.frame(mtry = seq(from = 22, to = 42, by = 5))
 
 rf_list <- list()
 ntrees <- c(500, 1000, 1500, 2000)
 
+start.time <- proc.time()
+cl <- makePSOCKcluster(6)
+registerDoParallel(cl)
+
 rf_list <- lapply(ntrees, function(ntree) {
+  cat("Computing for ntree = ", ntree)
   
   set.seed(123)
+  
   rf_train <- train(x = as.matrix(dtm_train),
                     y = as.factor(labels[train_partition]),
                     method = "rf",
-                    trControl = trainControl(method = "adaptive_cv", number = 10,
-                                             adaptive = list(min = 5, alpha = 0.05, 
-                                                             method = "gls", complete = TRUE),
-                                             search = "random"),
-                    tuneLength = 10,
+                    trControl = trainControl(method = "cv", number = 10),
+                    tuneGrid = mtry_grid,
                     ntree = ntree)
 })
 
 stop.time <- proc.time()
-print(stop.time - start.time) # 114.97 minutes
+print(stop.time - start.time)
 stopCluster(cl)
 env <- foreach:::.foreachGlobals
 rm(list=ls(name=env), pos=env)
 
-for (i in 1:4) {
-  print(plot(rf_list[[i]]))
-}
 
 rf_predict_ls <- list()
 
-cat("Forest with adaptive resampling",
+cat("Forest Unigrams",
     capture.output(
       for (i in 1:4) {
-        print(ntrees[i])
+        cat("ntrees =", ntrees[i], "; ",
+            "mtry =", rf_list[[i]][["bestTune"]][["mtry"]], "\n")
         rf_predict_ls[[i]] <- predict(rf_list[[i]], as.matrix(dtm_test))
         print(
           confusionMatrix(as.factor(rf_predict_ls[[i]]), as.factor(labels[-train_partition]),
                           mode = "everything")
         )
       }
-    ), file = "results/rf_resampling.txt", sep = "\n", append = TRUE
+    ), file = "results/results.txt", sep = "\n", append = TRUE
 )
 
-# OLD, without resampling
-
-mtry_grid <- data.frame(mtry = seq(from = 5, to = 20, by = 5))
-
-set.seed(123)
-
-rf_train <- train(x = as.matrix(dtm_train),
-                  y = as.factor(labels[train_partition]),
-                  method = "rf",
-                  trControl = trainControl(method = "cv", number = 10),
-                  tuneGrid = mtry_grid)
-
-rf_predict <- predict(rf_train, as.matrix(dtm_test))
-
-cat("Forest Unigrams",
-    capture.output(
-      confusionMatrix(as.factor(rf_predict), as.factor(labels[-train_partition]),
-                      mode = "everything")),
-    file = "results/results.txt", sep = "\n", append = TRUE)
 
 ### Bigrams
 
-set.seed(123)
-rf2_train <- train(x = as.matrix(dtm2_train),
-                   y = as.factor(labels[train_partition]),
-                   method = "rf",
-                   trControl = trainControl(method = "cv", number = 10),
-                   tuneGrid = mtry_grid)
+# mtry grid centered around sqrt(nfeat) ~ 40
+mtry_grid2 <- data.frame(mtry = seq(from = 30, to = 50, by = 5))
 
-rf2_predict <- predict(rf2_train, as.matrix(dtm2_test))
+rf2_list <- list()
+
+start.time <- proc.time()
+cl <- makePSOCKcluster(6)
+registerDoParallel(cl)
+
+rf2_list <- lapply(ntrees, function(ntree) {
+  cat("Computing for ntree = ", ntree)
+  
+  set.seed(123)
+  
+  rf2_train <- train(x = as.matrix(dtm2_train),
+                     y = as.factor(labels[train_partition]),
+                     method = "rf",
+                     trControl = trainControl(method = "cv", number = 10),
+                     tuneGrid = mtry_grid2)
+})
+
+stop.time <- proc.time()
+print(stop.time - start.time)
+stopCluster(cl)
+env <- foreach:::.foreachGlobals
+rm(list=ls(name=env), pos=env)
+
+
+rf2_predict_ls <- list()
 
 cat("Forest Bigrams",
     capture.output(
-      confusionMatrix(as.factor(rf2_predict), as.factor(labels[-train_partition]),
-                      mode = "everything")),
-    file = "results/results.txt", sep = "\n", append = TRUE)
+      for (i in 1:4) {
+        cat("ntrees =", ntrees[i], "; ",
+            "mtry =", rf2_list[[i]][["bestTune"]][["mtry"]], "\n")
+        rf2_predict_ls[[i]] <- predict(rf2_list[[i]], as.matrix(dtm2_test))
+        print(
+          confusionMatrix(as.factor(rf2_predict_ls[[i]]), as.factor(labels[-train_partition]),
+                          mode = "everything")
+        )
+      }
+    ), file = "results/results.txt", sep = "\n", append = TRUE
+)
 
 
 # Model comparison --------------------------------------------------------
---
+++ utils.R
@@ -0,0 +1,111 @@
+
+## Auxiliary functions for Data Mining Assignment 2 ## 
+
+
+# Lemmatization with tm ---------------------------------------------------
+
+get_lemmas <- function(x) {
+  lemmas <- udpipe(words(x), "english")[["lemma"]]
+  paste(lemmas, collapse = " ")
+}
+
+
+# Return unigrams and bigrams of a document -------------------------------
+
+get_unibigrams <- function(x) {
+  ngrams <- ngrams(words(x), 1:2)
+  return(unlist(lapply(ngrams, paste, collapse = " "), 
+                use.names = FALSE))
+}
+
+
+# Hyperparameter tuning of multinomial naive bayes ------------------------
+
+tune_mnb <- function(train_data){
+  
+  set.seed(123)
+  cv_folds <- crossv_kfold(data.frame(as.matrix(train_data)), k = 10)
+  
+  laplace_vals <- seq(0.1, 1, by = 0.1)
+  
+  # Training
+  models <- list()
+  
+  models <- lapply(cv_folds$train, function(fold) {
+    lapply(laplace_vals, function(l) {
+      multinomial_naive_bayes(x = as.matrix(fold$data[fold$idx,]), 
+                              y = as.factor(labels[fold$idx]),
+                              laplace = l)
+    })
+  })
+  
+  # Testing
+  predictions <- list()
+  
+  predictions <- lapply(models, function(m) {
+    lapply(1:length(laplace_vals), function(l) {
+      predict(m[[l]], as.matrix(as.data.frame(cv_folds$test[[l]])))
+    })
+  })
+  
+  accuracies <- list()
+  
+  accuracies <- lapply(1:length(predictions), function(fold) {
+    lapply(1:length(laplace_vals), function(l) {
+      mean(predictions[[fold]][[l]] == labels[cv_folds$test[[fold]][["idx"]]])
+    })
+  })
+  
+  # Average accuracy per Laplace value over folds 
+  ave_accuracies <- rowMeans(
+    # bind sub-lists into one data frame
+    do.call("rbind", 
+            # make sub-lists into data frame columns
+            lapply(accuracies, function(x) do.call("cbind", x))
+    )
+  )
+  
+  # identify best value for Laplace smoothing parameter 
+  return(best_laplace = laplace_vals[ave_accuracies == max(ave_accuracies)])
+  
+}
+
+
+# Feature extraction ------------------------------------------------------
+
+## MNB
+
+get_features_mnb <- function(train_model) {
+  
+  lapply(1:2, function(class) {
+    coefs <- coef(train_model)
+    coefs["feature"] <- rownames(coefs)
+    order <- order(coefs[class], decreasing = TRUE)
+    top5_df <- coefs[order[1:5], c(class, 3)]
+    top5 <- top5_df[[1]]
+    names(top5) <- top5_df[["feature"]]
+    top5
+  })
+}
+
+
+## LASSO
+
+get_features_lasso <- function(train_model, test_data) {
+  
+  coefs <- predict(train_model, newx = as.matrix(test_data), 
+                   s = "lambda.1se", type = "coefficient")
+  coef_names <- coefs@Dimnames[[1]]
+  coef_mat <- summary(coefs)
+  order <- order(coef_mat[,"x"], decreasing = TRUE)
+  
+  top5_true_mat <- coef_mat[order[1:5],]
+  top5_true_vals <- top5_true_mat[,"x"]
+  names(top5_true_vals) <- coef_names[top5_true_mat[,"i"]]
+  
+  top5_decep_mat <- coef_mat[rev(order)[1:5],]
+  top5_decep_vals <- top5_decep_mat[,"x"]
+  names(top5_decep_vals) <- coef_names[top5_decep_mat[,"i"]]
+  
+  list(deceptive = top5_decep_vals, truthful = top5_true_vals)
+}
